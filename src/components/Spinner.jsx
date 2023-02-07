2827687b21f8b03bfe7a8aafca0c0daad510c008
Alex <beccialex2002@gmail.com>
2023-02-07 11:12:40 -0300

    Loading in the search engine

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,10 +3,21 @@ import "../assets/css/spinner.css";
 
 function Spinner() {
   return (
-    <div className="lds-ripple container mx-96">
-      <div></div>
-      <div></div>
+    <div className="w-full flex items-center justify-center py-48">
+    <div className="xl:w-1/4 sm:w-1/2 w-full 2xl:w-1/5 flex flex-col items-center py-16 md:py-12  rounded-3xl">
+      <div className="w-full flex items-center justify-center">
+        <div className="flex flex-col items-center">
+          <div className="lds-ripple container mx-96">
+            <div></div>
+            <div></div>
+          </div>
+          <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-center text-white">
+            Cargando...
+          </p>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
 export default Spinner;
